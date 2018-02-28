dafcb84ca0ec7e5d0f4b4d1c5bf31c92172b9a0c
Ishan 'Fishy' Marikar <hello@imarikar.com>
2018-02-28 18:52:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Fake Facebook Hack
+
+**Current Status: Incomplete**
+
+![screenshot](https://i.imgur.com/bnhahFc.png)
+
+Started probably as a joke/collab idea by `kurtr` on DevRant (https://devrant.io/collabs/1179923) and I thought "Hey, I've made something
+similar. Why not do it?", and so I did. Although unlike the initial idea, this
+doesn't make use of OAuth but rather Facebook's internal API for messenger. Also,
+`-vim-` suggested, I turned this into a CLI app, unlike it's predecessor,
+which was based on Electron. I've tried to give it a very 'warez' feeling with a
+tad bit of inspiration from Metasploit.
+
+What it's supposed to do is basically:
+
+1. Log into the skiddie's Facebook.
+2. Allow him to choose the friends he wants to 'hack'.
+3. Pretend to 'hack' by displaying random content, logs and code to make it seem
+   like it's doing something.
+4. Message the people he tried to 'hack', stating that it's an automated message and that
+   said person attempted to hack .
+5. ...
+6. PROFIT.
+
+Do note that credentials don't actually touch anything besides Facebook's servers. This is not meant to be something used to harvest credentials. I'm not that evil.
---
+++ index.js
@@ -0,0 +1,123 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const ascii = require("ascii-art");
+const ora = require("ora");
+const async = require("async");
+const facebook = require("facebook-chat-api");
+
+inquirer.registerPrompt(
+  "autocomplete",
+  require("inquirer-autocomplete-prompt")
+);
+
+let API;
+
+const initialize = done => {
+  ascii.font("FB-HACK", "Doom", header => {
+    console.log(chalk.bold.green(header.trim()));
+    // Obviously fake handle and version number
+    console.log(chalk.gray("-- DeirkSeid\t\t\t\t\tv1.0\n"));
+    return done(null, true);
+  });
+};
+
+const checkIfConnectedToInternet = (data, done) => {};
+
+const getExploits = (data, done) => {
+  const spinner = ora("Looking for known exploits.").start();
+  setTimeout(() => {
+    spinner.stop();
+    return done(null, [
+      "CVE-2014-9524 (more likely to work)",
+      "CVE-2008-0660 (last resort)"
+    ]);
+  }, 1000);
+};
+
+const getCredentials = (data, done) => {
+  let validateInput = value => {
+    if (!value) {
+      return "This is a required step.";
+    } else if (value.trim().length <= 5) {
+      return "Not long enough.";
+    } else {
+      return true;
+    }
+  };
+
+  const questions = [
+    {
+      type: "list",
+      name: "method",
+      message: "Which exploit do you want to use?",
+      choices: data
+    },
+    {
+      type: "input",
+      name: "skiddieUsername",
+      message:
+        "What is your username/email? (this exploit requires authentication)",
+      validate: validateInput
+    },
+    {
+      type: "password",
+      name: "skiddiePassword",
+      message: "What is your username? (this exploit requires authentication)",
+      validate: validateInput
+    }
+  ];
+  inquirer.prompt(questions).then(answers => {
+    done(null, answers);
+  });
+};
+
+const facebookLogin = (data, done) => {
+  console.log("DEBUG:", data);
+  const spinner = ora("Connecting to facebook.com ...").start();
+  facebook(
+    {
+      email: data.skiddieUsername,
+      password: data.skiddiePassword
+    },
+    {
+      logLevel: "silent"
+    },
+    (error, api) => {
+      if (error) {
+        spinner.stop();
+        console.log(chalk.red("CRITICAL ERROR:", error.error));
+        process.exit(1);
+      }
+      API = api;
+      spinner.stop();
+      return done(null);
+    }
+  );
+};
+
+const getFriends = (data, done) => {
+  if (typeof API === "undefined") {
+    console.log(chalk.red("CRITICAL ERROR:", "Authentication Issue"));
+  }
+  const spinner = ora("Getting profile of friends..").start();
+  API.getFriendsList((error, friends) => {
+    if (error) console.log(chalk.red("CRITICAL ERROR:", error));
+    if (!friends || friends.length === 0) {
+      console.log(chalk.red("CRITICAL ERROR:", "Unable to fetch friend list."));
+    }
+    let validFriends = friends.filter(friend => {
+      return friend.userID !== "0";
+    });
+
+    // TODO: Get list of friends and have a list/dropdown/fuzzy-search to choose
+    // friend to "hack" from.
+  });
+};
+
+async.waterfall([
+  initialize,
+  getExploits,
+  getCredentials,
+  facebookLogin,
+  getFriends
+]);
